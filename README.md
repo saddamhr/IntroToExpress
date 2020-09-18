574d839f13397896aa132d378262d4d1763da55e
saddamhr <saddambubt65@gmail.com>
2020-09-18 09:19:29 +0600

    start a new project

---
+++ README.md
@@ -0,0 +1,2 @@
+# IntroToExpress
+# IntroToExpress
---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(`<h1>I am Listening!</h1>`)
+})
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=> {
+    console.log(`Server running on PORT ${PORT}`)
+})
